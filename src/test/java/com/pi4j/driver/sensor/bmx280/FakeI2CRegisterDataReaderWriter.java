84691aab7ec32bc71204cdff1be70e99ac9cc133
Stefan Haustein <stefan.haustein@gmail.com>
2025-08-20 21:00:59 +0200

    minor param name cleanup

--- src/test/java/com/pi4j/driver/sensor/bmx280/FakeI2CRegisterDataReaderWriter.java
+++ src/test/java/com/pi4j/driver/sensor/bmx280/FakeI2CRegisterDataReaderWriter.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Fake I2C register accees implementation that can play back / expect a given communication
+ * Fake I2C register access implementation that can play back / expect a given communication
  * pattern.
  * <p>
  * For the given test, a mock would have been more straightforward, but I didn't want to pull in
@@ -32,14 +32,14 @@ public class FakeI2CRegisterDataReaderWriter implements I2CRegisterDataReaderWri
     }
 
     @Override
-    public int readRegister(byte[] bytes, byte[] bytes1, int i, int i1) {
+    public int readRegister(byte[] register, byte[] data, int offset, int length) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public int readRegister(int index, byte[] data, int offset, int length) {
+    public int readRegister(int register, byte[] data, int offset, int length) {
         assertEquals(expectedCommunication[position++], READ, "READ marker expected");
-        assertEquals(expectedCommunication[position++], index, "register expected");
+        assertEquals(expectedCommunication[position++], register, "register expected");
 
         for (int i = 0; i < length; i++) {
             int input = expectedCommunication[position++];
@@ -51,16 +51,16 @@ public class FakeI2CRegisterDataReaderWriter implements I2CRegisterDataReaderWri
     }
 
     @Override
-    public int writeRegister(int index, byte value) {
+    public int writeRegister(int register, byte value) {
         buf[0] = value;
-        writeRegister(index, buf, 0, 1);
+        writeRegister(register, buf, 0, 1);
         return 1;
     }
 
     @Override
-    public int writeRegister(int index, byte[] data, int offset, int length) {
+    public int writeRegister(int register, byte[] data, int offset, int length) {
         assertEquals(expectedCommunication[position++], WRITE, "WRITE marker expected");
-        assertEquals(expectedCommunication[position++], index, "register expected");
+        assertEquals(expectedCommunication[position++], register, "register expected");
 
         for (int i = 0; i < length; i++) {
             int input = expectedCommunication[position++];
