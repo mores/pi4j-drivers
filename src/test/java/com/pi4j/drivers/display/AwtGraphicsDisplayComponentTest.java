81383db9a3d87c77ad79d766ba4a4856e6c7d9ab
Stephen More <stephen.more@gmail.com>
2025-09-03 20:50:08 -0400

    Updating the test.

--- src/test/java/com/pi4j/drivers/display/AwtGraphicsDisplayComponentTest.java
+++ src/test/java/com/pi4j/drivers/display/AwtGraphicsDisplayComponentTest.java
@@ -3,6 +3,15 @@ package com.pi4j.drivers.display;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
+import java.awt.image.DirectColorModel;
+import java.awt.image.Raster;
+import java.awt.image.SampleModel;
+import java.awt.image.SinglePixelPackedSampleModel;
+import java.awt.image.WritableRaster;
+
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
@@ -34,7 +43,6 @@ public class AwtGraphicsDisplayComponentTest {
         mockDisplay.display(img);
 
         byte[] data = display.getData();
-        byte zeroZero = data[0];
 
         log.trace("Size of Data: {}", data.length);
         assertEquals((byte) 0xF0, data[0], "First pixel is unexpected");
@@ -57,11 +65,68 @@ public class AwtGraphicsDisplayComponentTest {
         mockDisplay.display(img);
 
         byte[] data = display.getData();
-        byte zeroZero = data[0];
 
         log.trace("Size of Data: {}", data.length);
         assertEquals((byte) 0xF8, data[0], "First pixel is unexpected");
         assertEquals(10 * 10 * 16 / 8, data.length, "Length of data is unexpected");
     }
 
+    @Test
+    public void testDisplayDataBufferInt444() {
+
+        FakeDisplayDriver display = new FakeDisplayDriver(new DisplayInfo(10, 10, PixelFormat.RGB_444));
+        AwtGraphicsDisplayComponent mockDisplay = new AwtGraphicsDisplayComponent(display);
+
+        BufferedImage img = makeDataBufferInt();
+        mockDisplay.display(img);
+
+        byte[] data = display.getData();
+
+        log.trace("Size of Data: {}", data.length);
+        assertEquals(10 * 10 * 12 / 8, data.length, "Length of data is unexpected");
+        assertEquals((byte) 0xF0, data[0], "First pixel is unexpected");
+    }
+
+    @Test
+    public void testDisplayDataBufferInt565() {
+
+        FakeDisplayDriver display = new FakeDisplayDriver(new DisplayInfo(10, 10, PixelFormat.RGB_565));
+        AwtGraphicsDisplayComponent mockDisplay = new AwtGraphicsDisplayComponent(display);
+
+        BufferedImage img = makeDataBufferInt();
+        mockDisplay.display(img);
+
+        byte[] data = display.getData();
+
+        assertEquals(10 * 10 * 16 / 8, data.length, "Length of data is unexpected");
+        assertEquals((byte) 0xF8, data[0], "First pixel is unexpected");
+    }
+
+    private BufferedImage makeDataBufferInt() {
+        int width = 10, height = 10;
+
+        // Allocate pixel array
+        int[] pixels = new int[width * height];
+
+        // Wrap it in DataBufferInt
+        DataBufferInt dataBuffer = new DataBufferInt(pixels, pixels.length);
+
+        // Define how pixels are laid out
+        int[] bandMasks = { 0x00FF0000, 0x0000FF00, 0x000000FF }; // R, G, B
+        SampleModel sampleModel = new SinglePixelPackedSampleModel(DataBuffer.TYPE_INT, width, height, bandMasks);
+
+        WritableRaster raster = Raster.createWritableRaster(sampleModel, dataBuffer, null);
+
+        ColorModel colorModel = new DirectColorModel(24, 0x00FF0000, 0x0000FF00, 0x000000FF);
+
+        BufferedImage image = new BufferedImage(colorModel, raster, false, null);
+
+        // Example: set one pixel red
+        pixels[0] = 0x00FF0000;
+        pixels[1] = 0x00FF0000;
+        pixels[2] = 0x00FF0000;
+        pixels[3] = 0x00FF0000;
+
+        return image;
+    }
 }
